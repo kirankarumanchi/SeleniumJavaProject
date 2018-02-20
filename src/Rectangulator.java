2298377797e6f2d730d5c4d1a710555047fcac1c
kirankarumanchi <31979628+kirankarumanchi@users.noreply.github.com>
2018-02-20 15:23:42 +1000

    Adding various files

---
+++ src/Rectangulator.java
@@ -0,0 +1,13 @@
+public class Rectangulator
+{
+    public static void main(String[] args)
+    {
+        int length = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
+
+        Rectangle myRectangle = new Rectangle(length,width);
+        String output = "Length is: " + length + "Width is : " + width;
+
+        System.out.println(output);
+    }
+}
